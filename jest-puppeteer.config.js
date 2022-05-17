d102582426d99a096e9302602e1bbfa7e0bf712c
mathiastj <2278040+mathiastj@users.noreply.github.com>
2022-05-17 16:49:29 +0200

    Update jest puppeteer config for headless ci

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,22 +1,22 @@
-// module.exports = {
-//   launch: {
-//     executablePath: '/usr/bin/google-chrome-stable',
-//     headless: true,
-//     args: [
-//       '--ignore-certificate-errors',
-//       '--no-sandbox',
-//       '--disable-setuid-sandbox',
-//       '--disable-accelerated-2d-canvas',
-//       '--disable-gpu'
-//     ]
-//   }
-// }
-
 module.exports = {
   launch: {
-    dumpio: true,
-    headless: process.env.HEADLESS !== 'false',
-    product: 'chrome'
-  },
-  browserContext: 'default'
+    executablePath: '/usr/bin/google-chrome-stable',
+    headless: true,
+    args: [
+      '--ignore-certificate-errors',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-accelerated-2d-canvas',
+      '--disable-gpu'
+    ]
+  }
 }
+
+// module.exports = {
+//   launch: {
+//     dumpio: true,
+//     headless: process.env.HEADLESS !== 'false',
+//     product: 'chrome'
+//   },
+//   browserContext: 'default'
+// }
